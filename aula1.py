7f30b270010d0fc9eb9a6ed5c65fa50482d4b1a8
Wellinton - PC Klin <wellinton@null.net>
2025-01-06 09:21:50 -0300

    Comentários

--- aula1.py
+++ aula1.py
@@ -1,3 +1,10 @@
 print(1+1)
-# teste chat with server
+# comentarios em python
+"""
+  DocString em python servem como comentarios multi-linhas
+  que podem ser usados para documentar funções, classes, etc.
+  Porém, não é indicado a ser utilizado em códigos de produção
+
+  Pode ser aspas duplas ou simples
+"""
 print(2+2)
