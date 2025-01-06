7510a8bc99646fee33223f6ad68d01eede1bf211
Wellinton - PC Klin <wellinton@null.net>
2025-01-06 17:09:38 -0300

    If/else

--- aula16.py
+++ aula16.py
@@ -2,5 +2,7 @@ entrada = input('Teste if / else')
 
 if entrada == 'if':
   print('Entrada é igual a "if"')
+elif entrada == 'else':
+  print('Entrada é igual a "else"')
 else:
   print(f'Entrada não é igual a "if", ela é "{entrada}"')
