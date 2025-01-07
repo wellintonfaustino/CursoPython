c300f63bac1178aa95e32162528ceb97affbe2a8
Wellinton - PC Klin <wellinton@null.net>
2025-01-07 08:00:39 -0300

    Aula 20: ex. if/else

---
+++ aula20.py
@@ -0,0 +1,9 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+  print(f'{primeiro_valor} é maior que {segundo_valor}')
+else:
+  print(f'{primeiro_valor} é menor que {segundo_valor}')
+
