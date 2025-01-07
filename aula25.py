8b34faa9d825d22d2b8dd96a245263f89dbc3ced
Wellinton - PC Klin <wellinton@null.net>
2025-01-07 09:08:23 -0300

    aula 25: interpolação com strings

---
+++ aula25.py
@@ -0,0 +1,7 @@
+# Interpolação básica de strings
+
+nome = 'Wellinton'
+idade = 30
+
+print('Olá, ' + nome + '! Você tem ' + str(idade) + ' anos.')
+print ('Olá %s, sua idade é %.2f' % (nome, idade))
