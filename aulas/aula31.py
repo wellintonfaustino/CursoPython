f3faa66ba33f6411c60adb18d50084acb42f68cd
Wellinton - PC Klin <wellinton@null.net>
2025-01-07 13:44:28 -0300

    None

---
+++ aulas/aula31.py
@@ -0,0 +1,18 @@
+# Conditição
+# None
+
+condicao = False
+passou = None
+
+if condicao:
+  passou = True
+  print("Condição verdadeira")
+else:
+  passou = False
+  print("Condição falsa")
+
+print(passou)
+
+# Outro exemplo
+print(passou, passou is not None)
+print(passou, passou is None)
