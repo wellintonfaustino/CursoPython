1f00952b1a9d5cd0d71bab1b86b4550ec764d025
Wellinton - PC Klin <wellinton@null.net>
2025-01-07 08:07:14 -0300

    Aula21: operador and

---
+++ aula21.py
@@ -0,0 +1,10 @@
+# Operadores lógicos
+# and (e) ou (or) not (não)
+
+def teste_and ():
+  print(True and True) # True
+  print(True and False) # False
+  print(False and True) # False
+  print(False and False) # False
+
+teste_and()
