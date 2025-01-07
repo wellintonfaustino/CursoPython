349b067a8b856cb477d1d41f255c0fc7a5073bd3
Wellinton - PC Klin <wellinton@null.net>
2025-01-07 15:13:51 -0300

    Operadores de atribuição

---
+++ aulas/aula36.py
@@ -0,0 +1,39 @@
+"""
+  Operadores de atribuição
+  =, +=, -=, /=, %=, **=, //=
+  Atribui o valor da esquerda à variável da direita.
+"""
+
+a = 10
+
+print(a)
+
+a += 5
+
+print(a)
+
+a -= 3
+
+print(a)
+
+a *= 2
+
+print(a)
+
+a /= 2
+
+print(a)
+
+
+a **= 2
+
+print(a)
+
+a //= 5
+
+print(a)
+
+a %= 3
+
+print(a)
+
