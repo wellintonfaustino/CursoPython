ed9b04a6e7dd8491f142214da53df86a92e5b41c
Wellinton - PC Klin <wellinton@null.net>
2025-01-07 08:17:50 -0300

    Aula 24: in / not in

---
+++ aula24.py
@@ -0,0 +1,17 @@
+# Operadores in e not in
+
+lista = ['a', 'b', 'c', 'd', 'e']
+
+# Verifica se 'f' está presente na lista
+if 'f' in lista:
+  print('Sim, "f" esta presente na lista.')
+
+# Verifica se 'f' nao esta presente na lista
+if 'f' not in lista:
+  print('Nao, "f" nao esta presente na lista.')
+
+nome = 'Wellinton'
+
+# Verifica se 'W' esta presente no nome
+if 'W' in nome:
+  print('Sim, "W" esta presente no nome.')
