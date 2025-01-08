62f7bfa8deddfe305d6dcfb54d15110974331558
Wellinton - PC Klin <wellinton@null.net>
2025-01-08 14:18:47 -0300

    Desempacotamento

---
+++ aulas/aula50.py
@@ -0,0 +1,8 @@
+"""
+  Desempacotamento
+"""
+nomes = ['Maria','João','Paulo','John','Joana','Johnson']
+
+nome1, nome2, nome3, nome4, nome5, nome6 = nomes
+
+print(f'{nome1}, {nome2}, {nome3}, {nome4}, {nome5}, {nome6}')
