0e773cf19674bb04b125a50d5d969565ea31c21e
Wellinton - PC Klin <wellinton@null.net>
2025-01-08 10:01:28 -0300

    For

---
+++ aulas/aula43.py
@@ -0,0 +1,15 @@
+"""
+  Laço de repetição for
+"""
+
+for i in range(5):
+  print(i)
+
+texto = 'Wellinton'
+novo_texto = ''
+
+for letra in texto:
+  novo_texto += letra + ' - '
+  print(letra)
+
+print(novo_texto)
