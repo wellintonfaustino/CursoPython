62d3e915b3e7ccbe104ddd2f25f2861fed3080d4
Wellinton - PC Klin <wellinton@null.net>
2025-01-09 13:54:15 -0300

    Teste eddy

--- _exemplos/exemplo_texto_fala_pedge-tts.py
+++ _exemplos/exemplo_texto_fala_pedge-tts.py
@@ -2,9 +2,14 @@ import edge_tts
 import asyncio
 
 async def gerar_audio():
-    texto = "Você está panguándo"
-    comunicador = edge_tts.Communicate(texto, voice="pt-BR-FranciscaNeural")
+    texto = "É que eu aprendi a criar áudios, agora aquelas velhas que não sabem ler podem ouvir. Mas vou cobrar do Renan, obviamente! O Play dele ainda está pra jogo?"
+    comunicador = edge_tts.Communicate(texto, voice="pt-BR-ThalitaMultilingualNeural")
     await comunicador.save("audio.mp3")
+    """
+    pt-BR-AntonioNeural                Male      General                Friendly, Positive
+    pt-BR-FranciscaNeural              Female    General                Friendly, Positive
+    pt-BR-ThalitaMultilingualNeural    Female    General                Friendly, Positive
+    """
 
 asyncio.run(gerar_audio())
 print("Áudio salvo como audio.mp3")
