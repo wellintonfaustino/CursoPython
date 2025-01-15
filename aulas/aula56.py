ecf87bacc0b9708baed0df8a14ec3d23dc47bf5a
Wellinton - PC Klin <wellinton@null.net>
2025-01-15 07:48:52 -0300

    Aula 56

---
+++ aulas/aula56.py
@@ -0,0 +1,13 @@
+# Split e join
+
+# Split: Divide uma string em uma lista de substrings, separados por um separador
+frase = 'O Python eh uma linguagem de programacao multiparadigma. Linguagens multiplas podem ser usadas para resolver muitos problemas.'
+
+palavras = frase.split()
+
+print(palavras)
+
+# Join: Junta uma lista de substrings em uma string
+frase = ' '.join(palavras)
+
+print(frase)
